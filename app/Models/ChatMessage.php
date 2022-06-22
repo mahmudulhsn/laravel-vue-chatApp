a14c06f758bfda18912f8b9b3faae643f1042e39
M H Hasib <hasib.mahmudul@gmail.com>
2022-06-22 20:58:47 +0600

    create relationship

--- app/Models/ChatMessage.php
+++ app/Models/ChatMessage.php
@@ -2,12 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ChatMessage extends Model
 {
     use HasFactory;
 
     protected $fillable = ["char_room_id", "user_id", "message"];
+
+    /**
+     * Get the room associated with the ChatMessage
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function room(): HasOne
+    {
+        return $this->hasOne(ChatRoom::class);
+    }
+
+    /**
+     * Get the user associated with the ChatMessage
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
 }
--- app/Models/ChatRoom.php
+++ app/Models/ChatRoom.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ChatRoom extends Model
 {
     use HasFactory;
 
     protected $fillable = ["name"];
+
+    /**
+     * Get all of the messages for the ChatRoom
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function messages(): HasMany
+    {
+        return $this->hasMany(ChatMessage::class);
+    }
 }
