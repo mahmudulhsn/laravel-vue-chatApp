24258777ff2f790069f7f1aeeaf6508c1e549fa1
M H Hasib <hasib.mahmudul@gmail.com>
2022-06-22 20:55:39 +0600

    create chat room and char message wit hseeder

---
+++ database/seeders/ChatRoomSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ChatRoom;
+use Illuminate\Database\Seeder;
+
+class ChatRoomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        ChatRoom::upsert([
+            [
+                "name" => "General",
+            ],
+            [
+                "name" => "Tech",
+            ],
+        ], ["name"]);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::upsert([
+            [
+                "name" => "M H Hasib",
+                "email" => "hasib@hasib.com",
+                "email_verified_at" => now(),
+                "password" => "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", // password
+                "remember_token" => Str::random(10),
+            ],
+            [
+                "name" => "Mahmudul Hasan",
+                "email" => "mahmudul@mahmudul.com",
+                "email_verified_at" => now(),
+                "password" => "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", // password
+                "remember_token" => Str::random(10),
+            ],
+        ], ["email"]);
+    }
+}
