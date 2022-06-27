8da865fc9b2d7a683c5afd716f477e9753077957
M H Hasib <hasib.mahmudul@gmail.com>
2022-06-28 00:08:35 +0600

    fixed design

--- resources/js/Pages/Chat/InputMessage.vue
+++ resources/js/Pages/Chat/InputMessage.vue
@@ -2,10 +2,53 @@
   <div>
     <div class="border-t border-gray-500 py-2 px-5">
       <div class="flex justify-between">
+        <div class="w-1/12">
+          <div class="flex justify-center items-center w-full">
+            <label
+              for="dropzone-file"
+              class="
+                flex flex-col
+                justify-center
+                items-center
+                w-full
+                h-14
+                mr-2
+                bg-gray-50
+                rounded-lg
+                border-2 border-gray-300 border-dashed
+                cursor-pointer
+                dark:hover:bg-bray-800 dark:bg-gray-700
+                hover:bg-gray-100
+                dark:border-gray-600
+                dark:hover:border-gray-500
+                dark:hover:bg-gray-600
+              "
+            >
+              <div class="flex flex-col justify-center items-center pt-5 pb-6">
+                <svg
+                  class="mb-1 w-10 h-4 text-gray-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                  ></path>
+                </svg>
+                <p class="text-sm text-gray-500 dark:text-gray-400">Image</p>
+              </div>
+              <input id="dropzone-file" type="file" class="hidden" />
+            </label>
+          </div>
+        </div>
         <input
           type="text"
           class="
-            w-11/12
+            w-10/12
             focus:outline-0 focus:border-none
             border-none
             outline-0
--- resources/js/Pages/Chat/MessageItem.vue
+++ resources/js/Pages/Chat/MessageItem.vue
@@ -1,21 +1,62 @@
 <template>
-  <div class="text-sm text-gray-500 px-4 italic">
-    <div
-      :class="{ 'float-right': message.user_id == this.$page.props.user.id }"
-    >
-      <div class="text-yellow-600">
-        <span class="text-right">{{ message.user.name }}</span>
+  <div>
+    <div class="grid grid-cols-1 text-sm text-gray-500 px-4 italic">
+      <!-- name section -->
+      <div>
+        <div
+          class="text-yellow-600"
+          :class="{
+            'text-right mr-6': message.user_id == this.$page.props.user.id,
+            'text-left ml-6': message.user_id !== this.$page.props.user.id,
+          }"
+        >
+          <span class="text-right">{{ message.user.name }}</span>
+        </div>
       </div>
-      <div
-        class="w-fit rounded-2xl px-3 py-2 mx-5 text-lg text-white not-italic"
-        :class="{
-          'bg-green-600': message.user_id !== this.$page.props.user.id,
-          'bg-gray-600': message.user_id == this.$page.props.user.id,
-        }"
-      >
-        {{ message.message }}
+      <!-- name section -->
+
+      <!-- message section -->
+      <div>
+        <div
+          style="width: 80%"
+          :class="{
+            'float-right': message.user_id == this.$page.props.user.id,
+          }"
+        >
+          <div
+            class="
+              w-fit
+              rounded-2xl
+              px-3
+              py-2
+              mx-5
+              text-lg text-white
+              not-italic
+            "
+            :class="{
+              'bg-green-600': message.user_id !== this.$page.props.user.id,
+              'bg-gray-600': message.user_id == this.$page.props.user.id,
+              'float-right': message.user_id == this.$page.props.user.id,
+            }"
+          >
+            {{ message.message }}
+          </div>
+        </div>
       </div>
-      <div>{{ message.created_at }}</div>
+      <!-- message section -->
+
+      <!-- time -->
+      <div>
+        <div
+          :class="{
+            'text-right mr-6': message.user_id == this.$page.props.user.id,
+            'text-left ml-6': message.user_id !== this.$page.props.user.id,
+          }"
+        >
+          {{ message.created_at }}
+        </div>
+      </div>
+      <!-- time -->
     </div>
   </div>
 </template>
